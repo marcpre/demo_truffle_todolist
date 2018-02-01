3d3b2991923d3e6794c4afa95169f62346e0f297
marcpre <marcus.presich@gmail.com>
2018-02-01 17:22:42 +0100

    starting todolist contract with ether

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,46 +1,32 @@
-pragma solidity ^0.4.17;
+pragma solidity ^ 0.4.17;
 
 contract TodoList {
+
+    event NewTodo(uint todoId, string value);
+    event DeleteTodo(uint todoId, string value);
+
+    mapping(uint => address) public todoOwner;
+    mapping(address => uint) ownerTodoCount;
+
     TodoItem[] public todoItems;
-    
+
     struct TodoItem {
-        bytes32 value;
+        string value;
         //TODO submit ether to a TodoItem
         bool active;
     }
-    
-    function addTodoItem(bytes32 _value) returns(bool success) {
-        TodoItem memory todoItem;
-        todoItem.value = _value;
-        todoItem.active = false;
-        
-        todoItems.push(todoItem);
-        return true;
-    }
-    
-    function getTodoItem() returns (bytes32[], bool[]){
-        uint length = todoItems.length;
-        
-        //declare two arrays
-        bytes32[] memory values = new bytes32[](length);
-        bool[] memory actives = new bool[](length);
-        
-        for(uint i = 0; i < length; i++) {
-            values[i] = todoItems[i].value;
-            actives[i] = todoItems[i].active;
-        }
-        return (values, actives);
-    }
-    
-    function deleteTodoItem(uint index) returns(bool success) {
-        if(index >= todoItems.length) return;
-        
-        for (uint i = index; i < todoItems.length - 1; i++){
-            todoItems[i] = todoItems[i+1];
-        }
 
-        delete todoItems[todoItems.length - 1];
-        todoItems.length--;
-        return true;
+    function createTodo(string _value) internal {
+        uint id = todoItems.push(TodoItem(_value, true)) - 1;
+        todoOwner[id] = msg.sender;
+        ownerTodoCount[msg.sender]++;
+        NewTodo(id, _value);
     }
+    
+    //function deleteTodo;
+    
+    //function getAllTodos
+    
+    //function getTodoById
+    
 }
