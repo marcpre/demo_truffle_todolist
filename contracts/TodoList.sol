0864880b02382cf10b65d7b3231805becfbea690
marcpre <marcus.presich@gmail.com>
2018-02-02 05:46:28 +0000

    adding delte and getTodolist function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,8 +4,9 @@ import "./Ownable.sol";
 
 contract TodoList is Ownable {
 
-    event NewTodo(uint todoId, string value);
-    event DeleteTodo(uint todoId, string value);
+
+    event NewTodo(uint todoId, bytes32 value);
+    // event DeleteTodo(uint todoId, string value);
 
     //every user has an array of todo items
     mapping(uint => address) todoOwner;
@@ -15,15 +16,42 @@ contract TodoList is Ownable {
     TodoItem[] public todoItems;
 
     struct TodoItem {
-        string value;
+        bytes32 value;
         bool active;
     }
 
-    function createTodo(string _value) public returns(uint) {
+    function createTodo(bytes32 _value) public returns(bool) {
         uint id = todoItems.push(TodoItem(_value, true)) - 1;
         todoOwner[id] = msg.sender;
         ownerTodoCount[msg.sender]++;
         NewTodo(id, _value);
-        return id;
+        return true;
     }
+    
+    function getTodoItems() constant public returns (bytes32[], bool[]) {
+        uint length = todoItems.length;
+    
+        bytes32[] memory values = new bytes32[](length);
+        bool[] memory actives = new bool[](length);
+    
+        for (uint i = 0; i < length; i++) {
+          values[i] = todoItems[i].value;
+          actives[i] = todoItems[i].active;
+        }
+    
+        return (values, actives);
+    }
+    
+    function deleteTodoItem(uint index) public returns (bool success) {
+        if (index >= todoItems.length) return;
+
+        for (uint i = index; i < todoItems.length - 1; i++){
+            todoItems[i] = todoItems[i+1];
+        }
+
+        delete todoItems[todoItems.length - 1];
+        todoItems.length--;
+        return true;
+    }  
+    
 }
